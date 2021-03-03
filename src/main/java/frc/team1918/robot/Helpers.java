9ccedb44dfcf2876d87b817420d2483ce52bd73e
ncgears <1918ncgears@gmail.com>
2021-03-03 17:02:46 -0500

    Add radiansToTicks helper function

--- src/main/java/frc/team1918/robot/Helpers.java
+++ src/main/java/frc/team1918/robot/Helpers.java
@@ -14,7 +14,7 @@ public class Helpers {
            return inches * 0.0254;
        }
 
-        /** 
+        /**
          * This function takes encoder ticks and returns radians
          * @param ticks integer value in encoder ticks
          * @return value in radians
@@ -23,7 +23,16 @@ public class Helpers {
             return (ticks * Math.PI / (Constants.DriveTrain.DT_TURN_ENCODER_FULL_ROTATION / 2));
         }
 
-        /** 
+        /**
+         * This function takes radians and returns encoder ticks
+         * @param rads double precision value in radians
+         * @return integer value in encoder ticks
+         */
+        public final static int radiansToTicks(double rads) {
+			return (int) (rads / Math.PI * (Constants.DriveTrain.DT_TURN_ENCODER_FULL_ROTATION / 2));
+        }
+
+        /**
          * This function takes a value and modifies it by a gear reduction (or multiplication) for a single reduction
          * @param value double precision value of input
          * @param gearOne integer value of the number of teeth on the input side of the gear set (motor side)
@@ -33,7 +42,7 @@ public class Helpers {
         public final static double gearCalcSingle(double value, int gearOne, int gearTwo) {
             return value * (gearOne / gearTwo);
         }
-        /** 
+        /**
          * This function takes a value and modifies it by a gear reduction (or multiplication) for a single reduction
          * @param value double precision value of input
          * @param firstGearOne integer value of the number of teeth on the input side of the first gear set (motor side)
@@ -44,10 +53,10 @@ public class Helpers {
          */
         public final static double gearCalcDouble(double value, int firstGearOne, int firstGearTwo, int secondGearOne, int secondGearTwo) {
             return value * (firstGearOne / firstGearTwo) * (secondGearOne / secondGearTwo);
-        } 
-        
+        }
+
         /**
-         * This function takes a value in RPMs and converts it to meters Per Second.  
+         * This function takes a value in RPMs and converts it to meters Per Second.
          * @param rpm double precision value of RPMs from the motor controller
          * @return double precision value in meters per second based on wheel size and gear sets
          */
@@ -61,8 +70,8 @@ public class Helpers {
         private static Joystick oj = new Joystick(Constants.OI.OI_JOY_OPER);
         //DRIVER CONTROLS
         /**
-         * @param useDeadband Boolean value indicating whether to apply deadband to output 
-         * @return double precision -1 to 1 of the strafe axis 
+         * @param useDeadband Boolean value indicating whether to apply deadband to output
+         * @return double precision -1 to 1 of the strafe axis
          */
         public final static double getAxisStrafeValue(boolean useDeadband) {
             return (useDeadband) ? applyDeadband(dj.getRawAxis(Constants.OI.DRIVE_AXIS_STRAFE)) : dj.getRawAxis(Constants.OI.DRIVE_AXIS_STRAFE);
@@ -70,7 +79,7 @@ public class Helpers {
 
         /**
          * @param useDeadband Boolean value indicating whether to apply deadband to output
-         * @return double precision -1 to 1 of the fwd axis 
+         * @return double precision -1 to 1 of the fwd axis
          */
         public final static double getAxisFwdValue(boolean useDeadband) {
             return (useDeadband) ? applyDeadband(dj.getRawAxis(Constants.OI.DRIVE_AXIS_FWD)*-1) : dj.getRawAxis(Constants.OI.DRIVE_AXIS_FWD)*-1;
@@ -78,7 +87,7 @@ public class Helpers {
 
         /**
          * @param useDeadband Boolean value indicating whether to apply deadband to output
-         * @return double precision -1 to 1 of the turn axis 
+         * @return double precision -1 to 1 of the turn axis
          */
         public final static double getAxisTurnValue(boolean useDeadband) {
             return (useDeadband) ? applyDeadband(dj.getRawAxis(Constants.OI.DRIVE_AXIS_TURN)) : dj.getRawAxis(Constants.OI.DRIVE_AXIS_TURN);
@@ -134,7 +143,7 @@ public class Helpers {
         //OPERATOR CONTROLS
         /**
          * @param useDeadband Boolean value indicating whether to apply deadband to output
-         * @return double precision -1 to 1 of the fwd axis 
+         * @return double precision -1 to 1 of the fwd axis
          */
         public double getClimbAxisValue(boolean useDeadband) {
             return (useDeadband) ? applyDeadband(oj.getRawAxis(Constants.OI.OPER_AXIS_CLIMB)) : oj.getRawAxis(Constants.OI.OPER_AXIS_CLIMB);
@@ -149,7 +158,7 @@ public class Helpers {
         //HELPERS
         /**
          * @param inVal double precision input value to apply deadband
-         * @return double precision -1 to 1 after applying deadband calculation 
+         * @return double precision -1 to 1 after applying deadband calculation
          */
         public static final double applyDeadband(double inVal) {
             return ( Math.abs(inVal) < Constants.OI.OI_JOY_DEADBAND ) ? 0.0 : inVal;
@@ -159,7 +168,7 @@ public class Helpers {
          * This function zeros the joystick below the deadband. After the deadband, the value is normalized
          * from zero to 1
          * @param inVal double precision input value to apply deadband
-         * @return double precision -1 to 1 after applying deadband calculation 
+         * @return double precision -1 to 1 after applying deadband calculation
          */
         public static final double applyRampingDeadband(double inVal) {
             double ramped = inVal;
@@ -239,7 +248,7 @@ public class Helpers {
         //         @Override
         //         public boolean get() {
         //             return parent.getAngle() == direction.value;
-        //         }            
+        //         }
         //     }
 
         //     private int angle() {
