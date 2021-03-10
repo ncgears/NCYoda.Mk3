c91127e710b43507a9b3abf24b377d3f07cf416c
ncgears <1918ncgears@gmail.com>
2021-03-09 19:38:40 -0500

    Add finished conditions on shooter commands

--- src/main/java/frc/team1918/robot/commands/shooter/shooter_stopShooter.java
+++ src/main/java/frc/team1918/robot/commands/shooter/shooter_stopShooter.java
@@ -41,11 +41,12 @@ public class shooter_stopShooter extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    //stop the shooter
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
