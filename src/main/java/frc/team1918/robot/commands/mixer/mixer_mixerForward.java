f382563c80f8290765eae84136ac11c62a805fd7
ncgears <1918ncgears@gmail.com>
2021-03-09 19:22:22 -0500

    Add commands for mixer

---
+++ src/main/java/frc/team1918/robot/commands/mixer/mixer_mixerForward.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.team1918.robot.commands.mixer;
+
+import frc.team1918.robot.Helpers;
+import frc.team1918.robot.subsystems.MixerSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * A command that ...
+ */
+public class mixer_mixerForward extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"}) //Dont add "unused" under normal operation
+  private final MixerSubsystem m_subsystem;
+
+  /**
+   * @param subsystem The subsystem used by this command.
+   */
+  public mixer_mixerForward(MixerSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Helpers.General.debug("Mixer: Mixer Forward");
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
---
+++ src/main/java/frc/team1918/robot/commands/mixer/mixer_stopMixer.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.team1918.robot.commands.mixer;
+
+import frc.team1918.robot.Helpers;
+import frc.team1918.robot.subsystems.MixerSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * A command that ...
+ */
+public class mixer_stopMixer extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"}) //Dont add "unused" under normal operation
+  private final MixerSubsystem m_subsystem;
+
+  /**
+   * @param subsystem The subsystem used by this command.
+   */
+  public mixer_stopMixer(MixerSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Helpers.General.debug("Mixer: Stop Mixer");
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
