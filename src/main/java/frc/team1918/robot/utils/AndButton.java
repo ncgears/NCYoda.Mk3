17d50aa6f0be5d256b4c18d68f14e67935c177e4
ncgears <1918ncgears@gmail.com>
2021-03-16 15:48:07 -0400

    Add comments and licensing information to custom button classes

--- src/main/java/frc/team1918/robot/utils/AndButton.java
+++ src/main/java/frc/team1918/robot/utils/AndButton.java
@@ -1,3 +1,8 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2021 NC Gears Team 1918. All Rights Reserved.                */
+/* Open Source Software - may be modified and shared by FRC teams.            */
+/* No attribution is necessary to use this code                               */
+/*----------------------------------------------------------------------------*/
 package frc.team1918.robot.utils;
 
 import edu.wpi.first.wpilibj2.command.button.Button;
@@ -9,6 +14,11 @@ public class AndButton extends Button {
     private final Button button1;
     private final Button button2;
 
+    /**
+     * This function returns a Button object that is a combination of two other Buttons.  The get function requires both to be true.
+     * @param buttonOne The first button object
+     * @param buttonTwo The second button object
+     */
     public AndButton(Button buttonOne, Button buttonTwo) {
         button1 = buttonOne;
         button2 = buttonTwo;
